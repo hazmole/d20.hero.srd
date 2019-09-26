87eea543ee02b89cc5c38042bfadcdb4281ddc0d
Daniel.Huang <Daniel.Huang@infortrend.com>
2019-09-26 17:09:00 +0800

    type css

--- js/powereffects.js
+++ js/powereffects.js
@@ -11,17 +11,33 @@ IS_LOCAL_TEST = false;
 function getGeneralListItem (entry) {
 	var name = entry.translate_name? entry.translate_name: entry.name;
 	var type = Renderer.general.getTypeFullText(entry.type);
+	var type_class = getClassByType(entry.type);
 	var action = CUSTOM_RENDERER.getActionText(entry.action);
 	var range = Renderer.general.getRangeText(entry.range);
 	var duration = CUSTOM_RENDERER.getDurationText(entry.duration);
 	return `
 			<span class="name col-4">${name}</span>
-			<span class="type col-2 text-align-center">${type}</span>
+			<span class="type col-2 text-align-center ${type_class}">${type}</span>
 			<span class="action col-2 text-align-center">${action}</span>
 			<span class="range col-2 text-align-center">${range}</span>
 			<span class="duration col-2 text-align-center">${duration}</span>
 		`;
 }
+function getClassByType(type){
+	switch(type){
+		case "ATK": return "school_C";
+		case "CTL": return "school_T";
+		case "DEF": return "school_I";
+		case "MOV": return "school_D";
+		case "SEN": return "school_A";
+		case "C": return "school_C";
+		case "F": return "school_T";
+		case "G": return "school_A";
+		case "S": return "school_D";
+		default: return "";
+	}
+}
+
 function getFakeData(){
 	return {};
 }
